ac44faaffdd7f7253e11a49c8c41f50f0e921c91
sleopardo <santiagoleopardo@gmail.com>
2014-03-03 14:42:02 -0300

    Mis datos

--- js/mis-datos-ie.js
+++ js/mis-datos-ie.js
@@ -1,7 +1,7 @@
 function readURL(imgFile)
-{    
+{
     var newPreview = document.getElementById("preview_ie");
     newPreview.filters.item("DXImageTransform.Microsoft.AlphaImageLoader").src = imgFile.value;
-    newPreview.style.width = "160px";
-    newPreview.style.height = "120px";
+    newPreview.style.width = "100px";
+    newPreview.style.height = "100px";
 }
