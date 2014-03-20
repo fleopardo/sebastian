a9db8f386a8d905dcb736531efc0289409d87e34
sleopardo <santiagoleopardo@gmail.com>
2014-03-19 21:11:46 -0300

    placeholder

---
+++ js/global.js
@@ -0,0 +1,15 @@
+(function(){
+
+	//Placeholder fallback
+	if($("input[placeholder],textarea[placeholder]").length > 0){
+
+
+		$('input[placeholder],textarea[placeholder]').placeholder();
+
+	}
+
+})();
+
+
+
+
